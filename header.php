4e48c41bc0a96e64089f4df1190bc75b14be8f37
Alex <alexaverill2013@gmail.com>
2012-11-15 18:34:37 -0600

    Final Edits before testing

--- header.php
+++ header.php
@@ -225,6 +225,7 @@ if(strlen($_SESSION['admin_name'])>1 && $_SESSION['install']==$install_value){
 echo'<li><a href="index.php" class="menu_link">Team View</a></li>
 	<!--<li><a href="admin.php" class="menu_link"> Admin Area</a></li>-->
 	<li><a href="admin_manage.php" class="menu_link">Control  Users</a></li>
+  <li><a href="admin_manage.php" class="menu_link">Edit  Users</a></li>
 	<li><a href="admin_mail.php" class="menu_link">Emailing</a></li>
 	<li><a href="admin_addevent.php" class="menu_link">Control Events and Timer</a></li>
 	<li><a href="admin_event.php" class="menu_link">View Events</a></li>';
